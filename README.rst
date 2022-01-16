5e748b6c2db36fef8eb8cf64c97bf1a1318b1782
Segev Finer <segev208@gmail.com>
2022-01-16 15:21:23 +0200

    Add project metadata

---
+++ README.rst
@@ -0,0 +1,41 @@
+cyminhook
+=========
+.. image:: https://img.shields.io/pypi/v/cyminhook.svg
+   :target: https://pypi.org/project/cyminhook/
+   :alt: PyPI
+
+.. image:: https://github.com/segevfiner/cyminhook/actions/workflows/docs.yml/badge.svg
+   :target: https://segevfiner.github.io/cyminhook/
+   :alt: Docs
+
+Hook functions on Windows using MinHook.
+
+Quick Start:
+
+.. code-block:: python
+
+    import ctypes
+    import ctypes.wintypes
+    import cyminhook
+    import win32api
+    import win32con
+
+    signature = ctypes.WINFUNCTYPE(
+        ctypes.c_int,
+        ctypes.wintypes.HWND,
+        ctypes.wintypes.LPCWSTR,
+        ctypes.wintypes.LPCWSTR,
+        ctypes.wintypes.UINT,
+        ctypes.wintypes.WORD,
+        use_last_error=True,
+    )
+
+    target = ctypes.windll.user32.MessageBoxExW
+
+    def detour(hWnd, lpText, lpCaption, uType, langId):
+        return hook.original(hWnd, "Hooked", "Hooked", uType, langId)
+
+    with cyminhook.MinHook(signature=signature, target=target, detour=detour) as hook:
+        hook.enable()
+
+        win32api.MessageBox(None, "Hello, World!", "Python", win32con.MB_OK)
--- setup.py
+++ setup.py
@@ -1,10 +1,51 @@
+import re
 from skbuild import setup
 from setuptools import find_packages
 
 
+with open("cyminhook/__init__.py", "r", encoding="utf-8") as f:
+    version = re.search(r'(?m)^__version__ = "([a-zA-Z0-9.-]+)"', f.read()).group(1)
+
+with open("README.rst", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
 setup(
     name="cyminhook",
-    version="0.1.0",
+    version=version,
+    author="Segev Finer",
+    author_email="segev208@gmail.com",
+    description="Hook functions on Windows using MinHook",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/segevfiner/cyminhook",
+    project_urls={
+        "Documentation": "https://segevfiner.github.io/cyminhook/",
+        "Issue Tracker": "https://github.com/segevfiner/cyminhook/issues",
+    },
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Win32 (MS Windows)",
+        "Intended Audience :: Developers",
+        "Topic :: Security",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="MinHook",
     zip_safe=False,
     packages=find_packages(),
+    python_requires='>=3.6',
+    extras_require={
+        "dev": [
+            "sphinx==4.*",
+        ],
+    },
 )
